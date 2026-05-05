3a881df4fbad473958b5a95659dd474532f4cddd
Camron Gomes <c13gomes@student.bridgew.edu>
2026-05-05 18:40:34 -0400

    Devolved into what is essentailly now just a thin wrapper for Erick's "platformio_disable_upgrades" and the install_requires for dynamic dependencies.

--- setup.py
+++ setup.py
@@ -12,9 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 # Generate the flag for disabling upgrades
 upgrades_disabled_path = os.path.join(
@@ -28,77 +31,8 @@ else:
     if os.path.exists(upgrades_disabled_path):
         os.remove(upgrades_disabled_path)
 
-from platformio import (
-    __author__,
-    __description__,
-    __email__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-)
 from platformio.dependencies import get_pip_dependencies
 
 setup(
-    name=__title__,
-    version=__version__,
-    description=__description__,
-    long_description=open("README.rst").read(),
-    author=__author__,
-    author_email=__email__,
-    url=__url__,
-    license=__license__,
     install_requires=get_pip_dependencies(),
-    python_requires=">=3.6",
-    packages=find_packages(include=["platformio", "platformio.*"]),
-    package_data={
-        "platformio": [
-            "assets/system/99-platformio-udev.rules",
-            "project/integration/tpls/*/*.tpl",
-            "project/integration/tpls/*/.*.tpl",  # include hidden files
-            "project/integration/tpls/*/.*/*.tpl",  # include hidden folders
-            "project/integration/tpls/*/*/*.tpl",  # NetBeans
-            "project/integration/tpls/*/*/*/*.tpl",  # NetBeans
-        ]
-    },
-    entry_points={
-        "console_scripts": [
-            "platformio = platformio.__main__:main",
-            "pio = platformio.__main__:main",
-            "piodebuggdb = platformio.__main__:debug_gdb_main",
-        ]
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: C",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Software Development :: Compilers",
-    ],
-    keywords=[
-        "iot",
-        "embedded",
-        "arduino",
-        "mbed",
-        "esp8266",
-        "esp32",
-        "fpga",
-        "firmware",
-        "continuous-integration",
-        "cloud-ide",
-        "avr",
-        "arm",
-        "ide",
-        "unit-testing",
-        "hardware",
-        "verilog",
-        "microcontroller",
-        "debug",
-    ],
 )
